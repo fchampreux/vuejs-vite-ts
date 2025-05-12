a07a47a1bca830887f0a52744d61fc26cab5b2fa
fchampreux <fchampreux@gmail.com>
2025-05-12 16:49:28 +0200

    Updated router path and folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import './assets/styles/default.css';
 import App from './App.vue';
-import router from '@/router/router.ts';
+import router from '@/_router/router.ts';
 
 createApp(App)
 .use(router)
